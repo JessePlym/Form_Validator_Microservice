043ee49306d649335c553cc596104014505eda6a
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-11-03 12:09:35 +0200

    cors config made

---
+++ src/main/java/com/plymjesse/form_validator/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.plymjesse.form_validator.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  CorsConfigurationSource corsConfig() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:3000", "https://plymjesse.com"));
+    config.setAllowedHeaders(List.of("Content-Type"));
+    config.setAllowedMethods(List.of("POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/contact", config);
+    return source;
+  }
+}
