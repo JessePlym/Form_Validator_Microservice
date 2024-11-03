9efc4297d3f5325f4ea3b7d9fd7292c70121609e
Jesse Plym <bgu254@myy.haaga-helia.fi>
2024-11-03 16:17:09 +0200

    updated config

--- src/main/java/com/plymjesse/form_validator/config/WebConfig.java
+++ src/main/java/com/plymjesse/form_validator/config/WebConfig.java
@@ -11,15 +11,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class WebConfig {
 
-  // @Bean
-  // CorsConfigurationSource corsConfig() {
-  // CorsConfiguration config = new CorsConfiguration();
-  // config.setAllowedOrigins(List.of("https://plymjesse.com"));
-  // config.setAllowedHeaders(List.of("Content-Type"));
-  // config.setAllowedMethods(List.of("POST"));
-  // UrlBasedCorsConfigurationSource source = new
-  // UrlBasedCorsConfigurationSource();
-  // source.registerCorsConfiguration("/contact", config);
-  // return source;
-  // }
+  @Bean
+  CorsConfigurationSource corsConfig() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("https://plymjesse.com"));
+    config.setAllowedHeaders(List.of("Content-Type"));
+    config.setAllowedMethods(List.of("POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/contact", config);
+    return source;
+  }
 }
